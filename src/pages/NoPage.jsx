76578d404dd7f457fb17b19bbdce20ade09c0f28
Migueliyo <migueliyocolmenero@gmail.com>
2024-05-19 20:22:39 +0200

    Componente NoPage actualizado

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,5 +1,25 @@
+import { Box, Typography } from "@mui/material";
+
 const NoPage = () => {
-  return <h1 style={{ color: "#fff" }}>404</h1>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        style={{ width: 60, height: 60, marginRight: 10 }}
+        src="vite.svg"
+      ></img>
+      <Typography sx={{ width: 300, fontSize: 20, color: "#ABABAB" }}>
+        Lo sentimos. Este contenido no está disponible.
+      </Typography>
+    </Box>
+  );
 };
 
 export default NoPage;
